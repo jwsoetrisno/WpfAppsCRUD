6b7e437624ea4f2afb775e9bc31f8461db78e9fe
jwsoetrisno <128068497+jwsoetrisno@users.noreply.github.com>
2026-03-26 13:24:30 +0000

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using System.Windows;
+using WpfApps.Data;
+using WpfApps.Interface;
+using WpfApps.Services;
+using WpfApps.ViewModels;
+using WpfApps.Views;
+
+namespace WpfApps
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+
+        public App()
+        {
+            string database = ConfigurationManager.ConnectionStrings["LocalDatabase"].ConnectionString;
+
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddDbContext<AppDbContext>(options =>
+                        options.UseSqlite(database));
+
+                    services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+                    services.AddSingleton<ProductViewModel>();
+
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.DataContext = _host.Services.GetRequiredService<ProductViewModel>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            base.OnExit(e);
+        }
+    }
+
+}
