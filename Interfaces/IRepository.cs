5415ed6d6137aaf22bf7c164ec7973d8ded45362
jwsoetrisno <128068497+jwsoetrisno@users.noreply.github.com>
2026-03-26 13:41:14 +0000

    Add files via upload

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApps.Interface;
+
+public interface IRepository<T> where T : class
+{
+    Task<List<T>> GetAllAsync();
+    Task<T> GetAsync(int id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+}
