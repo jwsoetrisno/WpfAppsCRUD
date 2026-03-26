2fe7f26d84ebc2b2e24340b0d89c67110646bc90
jwsoetrisno <128068497+jwsoetrisno@users.noreply.github.com>
2026-03-26 13:39:43 +0000

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WpfApps.Models;
+
+namespace WpfApps.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
